98585e639d32b2ad965e323458573090ed3e44ff
kokoyroy <kokoyroy@gmail.com>
2021-08-22 21:57:33 +0300

    changing slowly

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import { createStage } from '../gameHelpers';
+import React from "react";
+import { createStage } from "../gameHelpers";
 // Components bellow
-import Stage from './Stage';
-import Display from './Display';
-import StartButton from './StartButton';
+import Stage from "./Stage";
+import Display from "./Display";
+import StartButton from "./StartButton";
+import { StyledTetriswrapper } from "./styles/StyledTetriswrapper";
+import { StyledTetris } from "./styles/StyledTetris";
 
 function Tetris() {
-    return (
-        <div>
-            <Stage  stage={createStage()}/>
-            <aside>
-                <div>
-                    <Display text="Score" />
-                    <Display text="Rows" />
-                    <Display text="Level" />
-                </div>
-                <StartButton callback="" />
-            </aside>
-        </div>
-    );
+  return (
+    <StyledTetriswrapper>
+      <StyledTetris>
+        <Stage stage={createStage()} />
+        <aside>
+          <div>
+            <Display text="Score" />
+            <Display text="Rows" />
+            <Display text="Level" />
+          </div>
+          <StartButton callback="" />
+        </aside>
+      </StyledTetris>
+    </StyledTetriswrapper>
+  );
 }
 
 export default Tetris;
---
+++ src/components/styles/StyledTetriswrapper.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import bgImage from "../../img/bg.png";
+
+export const StyledTetriswrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: url(${bgImage});
+  background-size: cover;
+  overflow: hidden;
+`;
