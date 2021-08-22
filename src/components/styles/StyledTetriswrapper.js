10a1106b1f1ca3424b79876e3a14e20c0d4b8bb9
kokoyroy <kokoyroy@gmail.com>
2021-08-22 22:13:44 +0300

    added a prettier bg

--- src/components/styles/StyledTetriswrapper.js
+++ src/components/styles/StyledTetriswrapper.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import bgImage from "../../img/bg.png";
+import bgImage from "../../img/bg.jpg";
 
 export const StyledTetriswrapper = styled.div`
   width: 100vw;
