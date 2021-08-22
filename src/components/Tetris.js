3e14238d5ecda8d7cc105a68ff317b34574307e6
kokoyroy <kokoyroy@gmail.com>
2021-08-23 01:24:27 +0300

    game board almost ready2

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,17 +1,29 @@
-import React from "react";
-import { createStage } from "../gameHelpers";
-// Components bellow
+import React, { useState } from "react";
+// Components
 import Stage from "./Stage";
 import Display from "./Display";
 import StartButton from "./StartButton";
 import { StyledTetriswrapper } from "./styles/StyledTetriswrapper";
 import { StyledTetris } from "./styles/StyledTetris";
+// hooks
+import { usePlayer } from "../hooks/usePlayer";
+import { useStage } from "../hooks/useStage";
 
 function Tetris() {
+  const [dropTime, setDropTime] = useState(null)
+  const [gameOver, setGameOver] = useState(false)
+  const [player] = usePlayer()
+  
+  const [stage, setStage, rowsCleared] = useStage(player);
+  console.log('rerender')
+
+
+
+
   return (
     <StyledTetriswrapper>
       <StyledTetris>
-        <Stage stage={createStage()} />
+        <Stage stage={stage} />
         <aside>
           <div>
             <Display text="Score" />
