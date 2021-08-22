13b99e77c165f2375af9a48925548f71eba14d4a
kokoyroy <kokoyroy@gmail.com>
2021-08-22 14:40:51 +0300

    added tetrominos

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -5,8 +5,6 @@ function Stage({ stage }) {
     return (
         <div>
             {stage.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]} />))}
-
-            {/* <Cell /> */}
         </div>
     );
 }
